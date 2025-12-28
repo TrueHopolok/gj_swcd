54c1853bb3c6589d18ff441f24465da396aad39f
TrueHopolok <vadim.truhanov@inbox.lv>
2025-12-28 23:38:46 +0200

    fixed logs

--- objects/log/log.gd
+++ objects/log/log.gd
@@ -4,12 +4,11 @@ extends Node2D
 @onready var pickup: SpatialAudioSource = $pickup
 @onready var oven: Oven = get_tree().get_first_node_in_group("Oven")
 
-var pickedup: bool = true
+var pickeable: bool = true
 
 func _on_button_pressed() -> void:
-	if pickedup:
-		if !oven.has_log:
-			pickup.play(0)
-			oven.has_log = true
-			visible = false
-		pickedup = false
+	if pickeable && !oven.has_log:
+		pickup.play(0)
+		oven.has_log = true
+		visible = false
+		pickeable = false
