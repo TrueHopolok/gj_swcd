b1ab87c2247c61b5847f2ba5d1d48a123788fd60
Richard Shahsalevich <richardsasalevich@outlook.com>
2026-01-16 20:15:23 +0200

    saving now hapepns after a change

--- ui/audio_settings/audio_slider.gd
+++ ui/audio_settings/audio_slider.gd
@@ -1,5 +1,7 @@
 extends HSlider
 
+var _main_menu: MainMenu
+
 @export var bus_name: StringName 
 
 var _bus_idx: int
@@ -7,8 +9,17 @@ var _bus_idx: int
 func _ready() -> void:
 	_bus_idx = AudioServer.get_bus_index(bus_name)
 	assert(_bus_idx >= 0, "Error: non existent bus name was set for audio slider")
-	value = AudioServer.get_bus_volume_linear(_bus_idx) * 100.0
+	_main_menu = get_tree().get_first_node_in_group("MainMenu")
+	_main_menu.read_config()
+	value = _main_menu.config.get_value("Audio", bus_name, 100.0)
+	drag_ended.connect(_on_drag_ended)
 
 
 func _on_value_changed(volume: float) -> void:
 	AudioServer.set_bus_volume_linear(_bus_idx, volume / 100.0)
+
+
+func _on_drag_ended(save: bool) -> void:
+	if save:
+		_main_menu.config.set_value("Audio", bus_name, value)
+		_main_menu.save_config()
